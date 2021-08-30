3f79b12225bda4be2abbfb753ebd4977bf6a473c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-08-29 23:33:31 -0300

    Car dto

---
+++ src/main/java/com/matheuscordeiro/kafkaproducer/producerapi/dto/CarDTO.java
@@ -0,0 +1,12 @@
+package com.matheuscordeiro.kafkaproducer.producerapi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CarDTO {
+    private String id;
+    private String model;
+    private String color;
+}
